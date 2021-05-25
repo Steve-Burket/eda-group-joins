5b3c87781664dd20399b9e2ea83223c3119cc1fa
Steve Burket <65436158+Steve-Burket@users.noreply.github.com>
2021-05-25 11:25:28 -0500

    Update joins-solution.sql

--- joins-solution.sql
+++ joins-solution.sql
@@ -1,4 +1,4 @@
-## Tasks
+--Tasks
 1. Get all customers and their addresses.
 2. Get all orders and their line items (orders, quantity and product).
 3. Which warehouses have cheetos?
@@ -8,7 +8,7 @@
 7. How many products do we carry?
 8. What is the total available on-hand quantity of diet pepsi?
 
-## Stretch
+--Stretch
 9. How much was the total cost for each order?
 10. How much has each customer spent in total?
 11. How much has each customer spent in total? Customers who have spent $0 should still show up in the table. It should say 0, not NULL (research coalesce).
